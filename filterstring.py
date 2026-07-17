55f9cc2ef25fbf573d4a92a0423a7caed9e68e10
mlaurent <mlaurent@f6r5s9.paris.42.school>
2026-07-17 17:43:23 +0200

    ft_filter and sos

--- filterstring.py
+++ filterstring.py
@@ -7,17 +7,27 @@ def main():
 
     if len(args) != 2:
         raise AssertionError("incorrect number of arguments")
-    
+
     try:
         number = int(args[1])
-    except:
+    except ValueError:
         raise AssertionError("argument is not an integer")
 
-    res = list(filter(lambda x: len(x) > number, args[0].split(" ")))
-    ft_res = list(ft_filter(lambda x: len(x) > number, args[0].split(" ")))
-    print(f"res = {res}")
-    print(f"ft_res = {ft_res}")
+    splitted = args[0].split(" ")
+
+    res = list(filter(lambda e: len(e) > number, splitted))
+    ft_res = list(ft_filter(lambda e: len(e) > number, splitted))
+    con_res = [e for e in splitted if len(e) > number]
+
+    print(f"filter =             {res}")
+    print(f"ft_filter =          {ft_res}")
+    print(f"list comprehension = {con_res}")
+
+    print(filter.__doc__)
+    print(ft_filter.__doc__)
+
     return 0
+
 
 if __name__ == "__main__":
     try:
--- ft_filter.py
+++ ft_filter.py
@@ -1,5 +1,6 @@
 def ft_filter(func, elements):
-    """Return an iterator yielding those items of iterable for which function(item) is true. 
+    """Return an iterator yielding those items of iterable for which
+    function(item) is true.
     If function is None, return the items that are true."""
     if func is None:
         return (e for e in elements if e)
